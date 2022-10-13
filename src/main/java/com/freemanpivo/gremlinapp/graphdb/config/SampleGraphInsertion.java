0da3efcd61385a446ae403c34c67b787673285ec
freemanpivo <andradepedroivo@gmail.com>
2022-10-12 23:50:38 -0300

    :beer: dumb refactoring

--- src/main/java/com/freemanpivo/gremlinapp/graphdb/config/SampleGraphInsertion.java
+++ src/main/java/com/freemanpivo/gremlinapp/graphdb/config/SampleGraphInsertion.java
@@ -5,7 +5,6 @@ import com.freemanpivo.gremlinapp.repository.ILikeRepository;
 import com.freemanpivo.gremlinapp.repository.IPersonRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,29 +12,29 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Slf4j
 public class SampleGraphInsertion implements CommandLineRunner {
-    private final IPersonRepository personRepository;
-    private final ILikeRepository likeRepository;
+    private final IPersonRepository peopleRepository;
+    private final ILikeRepository likesRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        personRepository.save(new Person("1234", "Pedro"));
-        personRepository.save(new Person("123", "Joao"));
-        personRepository.save(new Person("12", "Maria"));
-        personRepository.save(new Person("1", "Alambrado"));
+        peopleRepository.save(new Person("1234", "Pedro"));
+        peopleRepository.save(new Person("123", "Joao"));
+        peopleRepository.save(new Person("12", "Maria"));
+        peopleRepository.save(new Person("1", "Alambrado"));
 
-        likeRepository.save("1234", "1");
-        likeRepository.save("1234", "12");
-        likeRepository.save("1", "1234");
-        likeRepository.save("12", "1234");
+        likesRepository.save("1234", "1");
+        likesRepository.save("1234", "12");
+        likesRepository.save("1", "1234");
+        likesRepository.save("12", "1234");
 
-        final var person1 = personRepository.getById("PERSON#1234").get();
-        log.info("FOUND PEDRO ==> {}", personRepository.getById("PERSON#1234").get());
-        log.info("FOUND JOAO ==> {}", personRepository.getById("PERSON#123").get());
-        log.info("FOUND MARIA ==> {}", personRepository.getById("PERSON#12").get());
-        log.info("FOUND ALAMBRADO ==> {}", personRepository.getById("PERSON#1").get());
+        final var pedro = peopleRepository.getById("PERSON#1234").get();
+        log.info("FOUND PEDRO            ==> {}", peopleRepository.getById("PERSON#1234").get());
+        log.info("FOUND JOAO             ==> {}", peopleRepository.getById("PERSON#123").get());
+        log.info("FOUND MARIA            ==> {}", peopleRepository.getById("PERSON#12").get());
+        log.info("FOUND ALAMBRADO        ==> {}", peopleRepository.getById("PERSON#1").get());
 
-        final var pedroLikes = personRepository.peopleWhoLikes(person1);
-        log.info("PEOPLE LIKES PEDRO >>> {}", pedroLikes);
+        final var pedroLikes = peopleRepository.peopleWhoLikes(pedro);
+        log.info("PEOPLE WHO LIKES PEDRO ==> {}", pedroLikes);
 
         // TRICKY DATA => ELEMENT MAP vs VALUEMAP!
     }
